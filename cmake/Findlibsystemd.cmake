da8c890026561a772e9bdbbad95accc20a426c6f
capitalistspz <keipitalists@proton.me>
2026-06-22 07:36:13 +0100

    Add CMake package config, and simplify and correct find modules

--- cmake/Findlibsystemd.cmake
+++ cmake/Findlibsystemd.cmake
@@ -3,7 +3,7 @@ find_package(libsystemd CONFIG QUIET)
 if (NOT libsystemd_FOUND)
     find_package(PkgConfig QUIET)
     if (PKG_CONFIG_FOUND)
-        pkg_search_module(libsystemd IMPORTED_TARGET GLOBAL libsystemd)
+        pkg_search_module(libsystemd QUIET IMPORTED_TARGET libsystemd)
         if (libsystemd_FOUND)
             add_library(libsystemd::libsystemd ALIAS PkgConfig::libsystemd)
         endif ()
@@ -13,6 +13,4 @@ endif ()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(libsystemd
         REQUIRED_VARS
-        libsystemd_FOUND
-        libsystemd_LINK_LIBRARIES
-        VERSION_VAR libsystemd_VERSION)
+        libsystemd_FOUND)
